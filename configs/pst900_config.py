cf4a23fdda92d98c27dce6d05d7f3bdfe50c45c0
ynalcakan <yagiznalcakan@gmail.com>
2025-04-16 15:20:54 +0900

    config for pst900

--- configs/pst900_config.py
+++ configs/pst900_config.py
@@ -10,7 +10,7 @@ C = edict()
 config = C
 cfg = C
 
-C.seed = 12345
+C.seed = 42
 
 remoteip = os.popen('pwd').read()
 C.root_dir = os.path.abspath(os.path.join(os.getcwd(), './'))
@@ -36,38 +36,38 @@ C.is_test = False
 C.num_train_imgs = 597   # 597 2388
 C.num_eval_imgs = 288   # 288 1152
 C.num_classes = 5  # background (0) + 4 objects (1-4)
-C.class_names = ["unlabeled", "backpack", "fire extinguisher", "hand drill", "rescue randy"]
+C.class_names = ["unlabeled", "fire extinguisher", "backpack", "hand drill", "rescue randy"]
 
 """Image Config"""
-C.background = 0  # PST900 uses 0 as background class
+C.background = 255 
 C.image_height = 480
 C.image_width = 640
 C.norm_mean = np.array([0.485, 0.456, 0.406])
 C.norm_std = np.array([0.229, 0.224, 0.225])
 
 """ Settings for network, this would be different for each kind of model"""
-C.backbone = 'mit_b2' # Remember change the path below.   # Possibilities: mit_b0, mit_b1, mit_b2, mit_b3, mit_b4, mit_b5, swin_s, swin_b
+C.backbone = 'mit_b2_w_aspp' # Remember change the path below.   # Possibilities: mit_b0, mit_b1, mit_b2, mit_b3, mit_b4, mit_b5, swin_s, swin_b
 C.pretrained_model = C.root_dir + '/pretrained/segformer/mit_b2.pth'
-C.decoder = 'MLPDecoder'  # Possibilities: MLPDecoder, UPernet, deeplabv3+, None
+C.decoder = 'UPernet'  # Possibilities: MLPDecoder, UPernet, deeplabv3+, MLPDecoderpp, None
 C.decoder_embed_dim = 512
 C.optimizer = 'AdamW'
-C.criterion = 'CrossEntropyLoss'    # Possibilities: SigmoidFocalLoss, CrossEntropyLoss
+C.criterion = 'CrossEntropyLoss'    # Possibilities: CrossEntropyLoss, FocalLoss, DiceCELoss
 
-# SigmoidFocalLoss parameters
+# FocalLoss parameters
 C.FL_gamma = 4.0     
 C.FL_alpha = 0.25
 
 """Train Config"""
-C.lr = 6e-5
+C.lr = 2e-4
 C.lr_power = 0.9
 C.momentum = 0.9
-C.weight_decay = 0.01
-C.batch_size = 4
-C.nepochs = 15
+C.weight_decay = 5e-2   # use smaller if data is small
+C.batch_size = 8
+C.nepochs = 300
 C.niters_per_epoch = C.num_train_imgs // C.batch_size  + 1
 C.num_workers = 16
-C.train_scale_array = [0.5, 0.75, 1, 1.25, 1.5, 1.75]
-C.warm_up_epoch = 10
+C.train_scale_array = None #[0.5, 0.75, 1, 1.25, 1.5, 1.75]    # augmentation scale
+C.warm_up_epoch = 10         # lr warm up epoch
 
 C.fix_bias = True
 C.bn_eps = 1e-3
@@ -81,7 +81,7 @@ C.eval_flip = False # True #
 C.eval_crop_size = [480, 640] # [height weight]
 
 """Store Config"""
-C.checkpoint_start_epoch = 350
+C.checkpoint_start_epoch = 50
 C.checkpoint_step = 50
 
 """Path Config"""
@@ -90,10 +90,10 @@ def add_path(path):
         sys.path.insert(0, path)
 add_path(osp.join(C.root_dir))
 
-if C.criterion == 'SigmoidFocalLoss':
-    log_path = 'logs/' + C.dataset_name + '/' + 'log_' + C.backbone + '_' + C.decoder + '_IFRM' + '_' + C.criterion + '_gamma' + str(C.FL_gamma) + '_alpha' + str(C.FL_alpha)
+if C.criterion == 'FocalLoss':
+    log_path = 'logs/' + C.dataset_name + '/' + 'log_' + C.backbone + '_' + C.decoder + '_FRM' + '_' + C.criterion + '_gamma' + str(C.FL_gamma) + '_alpha' + str(C.FL_alpha)
 else:
-    log_path = 'logs/' + C.dataset_name + '/' + 'log_' + C.backbone + '_' + C.decoder + '_IFRM' + '_' + C.criterion + '_ssl_thermal'
+    log_path = 'logs/' + C.dataset_name + '/' + 'log_' + C.backbone + '_' + C.decoder + '_FRM' + '_' + C.criterion 
 
 C.log_dir = osp.abspath(log_path)
 C.tb_dir = osp.abspath(osp.join(C.log_dir, "tb"))
