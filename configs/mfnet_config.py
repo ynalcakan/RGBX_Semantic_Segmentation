307152a438b72fefc53d6e0f3e7fcf2656664283
ynalcakan <yagiznalcakan@gmail.com>
2025-04-30 10:56:09 +0900

    config for FMB dataset

---
+++ configs/mfnet_config.py
@@ -0,0 +1,154 @@
+import os
+import os.path as osp
+import sys
+import time
+import numpy as np
+from easydict import EasyDict as edict
+import argparse
+
+C = edict()
+config = C
+cfg = C
+
+C.seed = 12345
+
+remoteip = os.popen('pwd').read()
+C.root_dir = os.path.abspath(os.path.join(os.getcwd(), './'))
+C.abs_dir = osp.realpath(".")
+
+
+# Dataset config
+"""Dataset Path"""
+C.dataset_name = 'MFNet'
+C.dataset_path = osp.join(C.root_dir, 'datasets', 'MFNet')
+C.rgb_root_folder = osp.join(C.dataset_path, 'RGB')
+C.rgb_format = '.png'
+C.gt_root_folder = osp.join(C.dataset_path, 'Label')
+C.gt_format = '.png'
+C.gt_transform = False
+# True when label 0 is invalid, you can also modify the function _transform_gt in dataloader.RGBXDataset
+# True for most dataset valid, False for MFNet(?)
+C.x_root_folder = osp.join(C.dataset_path, 'Thermal')
+C.x_format = '.png'
+C.x_is_single_channel = True # True for raw depth, thermal and aolp/dolp(not aolp/dolp tri) input
+C.train_source = osp.join(C.dataset_path, "train_val.txt")
+C.eval_source = osp.join(C.dataset_path, "test.txt")
+C.is_test = False
+C.num_train_imgs = 1176
+C.num_eval_imgs = 393
+C.num_classes = 9
+C.class_names =  ["Unlabeled", "Car", "Person", "Bike", "Curve", "Car Stop", "Guardrail", "Color Cone", "Bump"]
+
+# Class balancing parameters
+C.beta = 0.9999  # For ClassBalancedCELoss
+C.ma_momentum = 0.9  # For MABalancedCELoss
+
+"""Image Config"""
+C.background = 255
+C.image_height = 480
+C.image_width = 640
+C.norm_mean = np.array([0.485, 0.456, 0.406])
+C.norm_std = np.array([0.229, 0.224, 0.225])
+
+""" Settings for network, this would be different for each kind of model"""
+C.backbone = 'mit_b2' # Remember change the path below.   # Possibilities: mit_b0, mit_b1, mit_b2, mit_b3, mit_b4, mit_b5, swin_s, swin_b
+C.pretrained_model = C.root_dir + '/pretrained/segformer/mit_b2.pth'
+C.decoder = 'UPernet'  # Possibilities: MLPDecoder, UPernet, deeplabv3+, None
+C.decoder_embed_dim = 512
+C.rectify_module = 'FRM'  # Possibilities: FRM, IFRM
+C.fusion_module = 'FFM'  # Possibilities: FFM, IFFM
+C.optimizer = 'AdamW'
+C.criterion = 'CE_SoftEdgeLoss'    # Possibilities: SigmoidFocalLoss, CrossEntropyLoss, ClassBalancedCELoss, BatchBalancedCELoss, MABalancedCELoss, MedianFreqCELoss, SoftEdgeLoss, CE_SoftEdgeLoss
+
+# SigmoidFocalLoss parameters
+C.FL_gamma = 4.0     
+C.FL_alpha = 0.25
+
+"""Loss function Config"""
+# WeightedCrossEntropy2d parametersex
+# C.class_weights = [0.6, 0.9, 1.0, 1.4, 1.2, 1.5, 1.7, 1.4, 1.2] 
+# C.class_weights = [0.8, 0.9, 1.2, 1.5, 1.2, 1.5, 1.0, 1.8, 1.5] # previous
+# C.class_weights = [0.2 , 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+# -> C.class_weights = [6.09292401e-04, 1.33798871e-01, 4.56693120e-01, 6.42304688e-01, 9.05816050e-01, 1.11604266e+00, 4.60961076e+00, 2.58301822e+00, 1.66006424e+00]
+
+
+# Focal_dice_loss parameters
+C.FDL_alpha = 0.5 # Focal loss weight
+C.FDL_beta = 0.5 # Dice loss weight
+C.FDL_eps = 1e-6 # Dice loss epsilon
+
+"""LR Config"""
+C.lr_method = 'CosineAnnealingWarmupLR' # 'OneCycleLR', 'WarmUpPolyLR', 'StepLR', 'CosineAnnealingWarmupLR', 'ReduceLROnPlateauLR', 'CyclicLR', 'MultiStageLR', 'LinearIncreaseLR'
+C.lr = 1e-4  # Slightly higher than current 3e-5 for better exploration
+C.lr_power = 0.8 # 0.9  
+C.warm_up_epoch = 25  # ~5-6% of total epochs (450) is optimal for warmup
+C.min_lr = 5e-6  # 5% of max learning rate prevents too small gradients
+C.factor = 0.1
+C.patience = 10
+C.threshold = 1e-4
+C.cooldown = 0
+C.cycle_epochs = 1
+C.warmup_epochs = 1
+C.weight_decay = 0.01  # Reduce slightly from 0.015 for cosine scheduler
+
+"""Train Config"""
+C.momentum = 0.9
+C.batch_size = 12 # 8
+C.nepochs = 500
+C.niters_per_epoch = C.num_train_imgs // C.batch_size  + 1
+C.num_workers = 16
+C.train_scale_array = [0.5, 0.75, 1, 1.25, 1.5, 1.75]
+
+# Data augmentation options
+C.enable_random_mirror = True         # Enable/disable horizontal flipping during training
+C.enable_random_crop = False           # Enable/disable random crop during training
+C.enable_color_jitter = False          # Enable/disable color jittering
+C.enable_gaussian_blur = False         # Enable/disable Gaussian blur
+C.enable_cutout = False                # Enable/disable cutout augmentation
+
+C.fix_bias = True
+C.bn_eps = 1e-3
+C.bn_momentum = 0.1
+
+"""Eval Config"""
+C.eval_iter = 25
+C.eval_stride_rate = 2 / 3
+C.eval_scale_array = [1] # [0.75, 1, 1.25] # 
+C.eval_flip = False # True # 
+C.eval_crop_size = [480, 640] # [height weight]
+
+"""Store Config"""
+C.checkpoint_start_epoch = 100
+C.checkpoint_step = 50
+
+"""Path Config"""
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+add_path(osp.join(C.root_dir))
+
+if C.criterion == 'SigmoidFocalLoss':
+    log_path = 'logs/' + C.dataset_name + '/' + 'log_' + C.backbone + '_' + C.decoder + '_' + C.rectify_module + '_' + C.fusion_module + '_' + C.criterion + '_gamma' + str(C.FL_gamma) + '_alpha' + str(C.FL_alpha)
+else:
+    log_path = 'logs/' + C.dataset_name + '/' + 'log_' + C.backbone + '_' + C.decoder + '_' + C.rectify_module + '_' + C.fusion_module + '_' + C.criterion
+
+C.log_dir = osp.abspath(log_path)
+C.tb_dir = osp.abspath(osp.join(C.log_dir, "tb"))
+C.log_dir_link = C.log_dir
+C.checkpoint_dir = osp.abspath(osp.join(C.log_dir, "checkpoint"))
+
+exp_time = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
+C.log_file = C.log_dir + '/log_' + exp_time + '.log'
+C.link_log_file = C.log_file + '/log_last.log'
+C.val_log_file = C.log_dir + '/val_' + exp_time + '.log'
+C.link_val_log_file = C.log_dir + '/val_last.log'
+
+if __name__ == '__main__':
+    print(config.nepochs)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-tb', '--tensorboard', default=False, action='store_true')
+    args = parser.parse_args()
+
+    if args.tensorboard:
+        open_tensorboard()
